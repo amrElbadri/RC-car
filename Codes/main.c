6609a73d41b80733d9a30aba569596c20d8e7be2
mohamedrefaaat <36375076+mohamedrefaaat@users.noreply.github.com>
2018-02-22 01:37:09 +0200

    Add files via upload
    SERVO  motor code

--- Codes/main.c
+++ Codes/main.c
@@ -1,22 +1,44 @@
-#define F_CPU 1000000UL
 #include <avr/io.h>
+
 #include <util/delay.h>
-#include "MACROS.h"
-int main(void)
+
+//Simple Wait Function
+void Wait()
 {
-   DDRA=0X3F;
-    while (1) 
-    {
-		forward();
-		_delay_ms(10000);
-		backward();
-		_delay_ms(10000);
-		right();
-		_delay_ms(100);
-		left();
-		_delay_ms(100);
-		stop();
-		_delay_ms(100);
-    }
+	uint8_t i;
+	for(i=0;i<50;i++)
+	{
+		_delay_loop_2(0);
+		_delay_loop_2(0);
+		_delay_loop_2(0);
+	}
+
 }
 
+void main()
+{
+	//Configure TIMER1
+	TCCR1A|=(1<<COM1A1)|(1<<COM1B1)|(1<<WGM11);        //NON Inverted PWM
+	TCCR1B|=(1<<WGM13)|(1<<WGM12)|(1<<CS11)|(1<<CS10); //PRESCALER=64 MODE 14(FAST PWM)
+
+	ICR1=4999;  //fPWM=50Hz (Period = 20ms Standard).
+
+	DDRD|=(1<<PD4)|(1<<PD5);   //PWM Pins as Out
+
+	while(1)
+	{
+
+		OCR1A=97;   //0 degree
+		Wait();
+
+		OCR1A=316;  //90 degree
+		Wait();
+
+		OCR1A=425;  //135 degree
+
+		Wait();
+
+		OCR1A=535;  //180 degree
+		Wait();
+	}
+}
