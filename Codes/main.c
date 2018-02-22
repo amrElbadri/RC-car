fa12d8114a260a21f432fec3d6b8ea8ee83f4811
mohamedrefaaat <36375076+mohamedrefaaat@users.noreply.github.com>
2018-02-22 02:01:37 +0200

    Add files via upload
    4 led bouncing

--- Codes/main.c
+++ Codes/main.c
@@ -1,44 +1,19 @@
-#include <avr/io.h>
-
-#include <util/delay.h>
-
-//Simple Wait Function
-void Wait()
+#define F_CPU 1000000UL
+#include <avr/io.h> 
+#include <avr/delay.h>
+int main(void) 
 {
-	uint8_t i;
-	for(i=0;i<50;i++)
-	{
-		_delay_loop_2(0);
-		_delay_loop_2(0);
-		_delay_loop_2(0);
-	}
-
-}
-
-void main()
-{
-	//Configure TIMER1
-	TCCR1A|=(1<<COM1A1)|(1<<COM1B1)|(1<<WGM11);        //NON Inverted PWM
-	TCCR1B|=(1<<WGM13)|(1<<WGM12)|(1<<CS11)|(1<<CS10); //PRESCALER=64 MODE 14(FAST PWM)
-
-	ICR1=4999;  //fPWM=50Hz (Period = 20ms Standard).
-
-	DDRD|=(1<<PD4)|(1<<PD5);   //PWM Pins as Out
-
+	DDRA=0B11110000;
 	while(1)
 	{
-
-		OCR1A=97;   //0 degree
-		Wait();
-
-		OCR1A=316;  //90 degree
-		Wait();
-
-		OCR1A=425;  //135 degree
-
-		Wait();
-
-		OCR1A=535;  //180 degree
-		Wait();
+		PORTA=0b10000000;
+		_delay_ms(500);
+		PORTA=0b01000000;
+		_delay_ms(500);
+		PORTA=0b00100000;
+		_delay_ms(500);
+		PORTA=0b00010000;
+		_delay_ms(500);
 	}
-}
+	return 0 ;
+} 
