fd8748bff266a0b442396ae45cc3242b7107ab1b
Solariii <36302669+Solariii@users.noreply.github.com>
2018-02-25 18:40:40 +0200

    Add files via upload

--- Codes/main.c
+++ Codes/main.c
@@ -1,19 +1,48 @@
-#define F_CPU 1000000UL
-#include <avr/io.h> 
-#include <avr/delay.h>
-int main(void) 
+#include <avr/io.h>
+
+#define get_bit(reg,bitnum) ((reg & (1<<bitnum))>>bitnum)
+
+int main(void)
+
 {
-	DDRA=0B11110000;
+
+	DDRD=0b11111000; // set the first 3 pins of PORTD to be inputs to read from the push buttons
+
+	DDRB=0b11111111; // ensure that Pin3 in POrt B is output as this is the OC0 pin that wll produce the PWM.
+
+	PORTD=0b00000000; // Initialize PORTD to zeros
+
+	TCCR0=0b01110101; //Configure TCCR0 as explained in the article
+
+	TIMSK=0b00000000;
+
+	OCR0=255; // Set OCR0 to 255 so that the duty cycle is initially 0 and the motor is not rotating
+
 	while(1)
+
 	{
-		PORTA=0b10000000;
-		_delay_ms(500);
-		PORTA=0b01000000;
-		_delay_ms(500);
-		PORTA=0b00100000;
-		_delay_ms(500);
-		PORTA=0b00010000;
-		_delay_ms(500);
-	}
-	return 0 ;
-} 
+
+		if (get_bit(PIND,0)==1)
+
+		{
+
+			OCR0=178; //If button 1 is pressed, set OCR0=178 (duty cycle=10%).
+
+		}
+
+		if (get_bit(PIND,1)==1)
+
+		{
+
+			OCR0=102; //If button 2 is pressed, set OCR0=102 (duty cycle=60%).
+
+		}
+
+		if (get_bit(PIND,2)==1)
+
+		{
+
+			OCR0=25; //If button 3 is pressed, set OCR0=25 (duty cycle=90%).
+
+		}      }  }
+
