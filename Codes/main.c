d0dc5abdef61d2a38eba8bd5381ba170f6140eb2
Solariii <36302669+Solariii@users.noreply.github.com>
2018-03-13 23:19:25 +0200

    Add files via upload
    Develop an embedded system connected to 8 bit dip-switches. The system produce a pulse of duration
    equal to the supplied 8 bit value (0255) multiplied by 4.

--- Codes/main.c
+++ Codes/main.c
@@ -1,67 +1,19 @@
-#include "MACROS.h"
+
 #include <avr/io.h>
-void Initpwm()
+#define GET_BIT(reg,Index)  ((reg>>Index)&0x01)
+
+int main(void)
 {
-	
-	TCCR0=0b11110101;
-	TIMSK=0b00000000;
-	OCR0=255; // Set OCR0 to 255 so that the duty cycle is initially 0 and the motor is not rotating
+    DDRD=0x00;
+	DDRA=0xff;
+    while (1) 
+    {
+		int f=PIND;
+		for(int i=0; i<(f*4); i++)
+		{
+			PORTA=0x01;
+		}
+		PORTA=0x00;
+    }
 }
-void Initpwm1()
-{
-	
-	TCCR0=0b11110101;
-	TIMSK=0b00000000;
-	OCR2=60; // Set OCR0 to 255 so that the duty cycle is initially 0 and the motor is not rotating
-}
-void forward()
-{
-	SET_BIT(PORTA,mRP);
-	CLR_BIT(PORTA,mRN);
-	SET_BIT(PORTA,mLN);
-	CLR_BIT(PORTA,mLP);
-	
-	
-}
-void backward()
-{
-	CLR_BIT(PORTA,mRP);
-	SET_BIT(PORTA,mRN);
-	SET_BIT(PORTA,mLN);
-	CLR_BIT(PORTA,mLP);
-}
-void right()
-{
-	SET_BIT(PORTA,mRP);
-	CLR_BIT(PORTA,mRN);
-	SET_BIT(PORTA,mLN);
-	CLR_BIT(PORTA,mLP);
-}
-void left()
-{
-	CLR_BIT(PORTA,mRP);
-	SET_BIT(PORTA,mRN);
-	CLR_BIT(PORTA,mLN);
-	SET_BIT(PORTA,mLP);
-}
-void SlightLeft()
-{
-	CLR_BIT(PORTA,mRP);
-	CLR_BIT(PORTA,mRN);
-	CLR_BIT(PORTA,mLN);
-	SET_BIT(PORTA,mLP);
-}
-void SlightRight()
-{
-	SET_BIT(PORTA,mRP);
-	CLR_BIT(PORTA,mRN);
-	CLR_BIT(PORTA,mLN);
-	CLR_BIT(PORTA,mLP);
-}
-void stop()
-{
-	CLR_BIT(PORTA,mRP);
-	CLR_BIT(PORTA,mRN);
-	CLR_BIT(PORTA,mLN);
-	CLR_BIT(PORTA,mLP);
-}
+
