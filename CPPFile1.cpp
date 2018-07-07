9ae2491a2dcc0bc3a45e93156fe88ad018463ce0
OmarEid73 <36411638+OmarEid73@users.noreply.github.com>
2018-07-08 01:29:26 +0200

    Add files via upload

---
+++ CPPFile1.cpp
@@ -0,0 +1,15 @@
+﻿#define  F_CPU 16000000UL
+#define get_bit(reg,index)  ((reg>>index)&0x01)
+#include <avr/io.h>
+#include <util/delay.h>
+static volatile double pulse = 0;
+int main(void)
+{
+	DDRC=0b11011111;
+	while (1)
+{
+	PORTC|=(1<<6);
+	_delay_ms(1000);
+	PORTC &=~(1<<6);
+	_delay_ms(1000);
+}
